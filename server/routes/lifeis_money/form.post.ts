8dcc5667abc2e7dcf28b63bf694f534c416e3c48
Wing <git@public.wing.osaka>
2026-03-07 07:54:49 +0000

    受信データ

--- server/routes/lifeis_money/form.post.ts
+++ server/routes/lifeis_money/form.post.ts
@@ -3,5 +3,5 @@ import { eventHandler, readBody } from 'h3';
 export default eventHandler(async (event) => {
   const body = await readBody(event);
 
-  console.log(body);
+  console.log("受信データ", body);
 });
