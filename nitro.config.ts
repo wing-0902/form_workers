86b5620d72903b1a8b2c52e45d180b58b3af3e67
Wing <git@public.wing.osaka>
2026-03-07 06:48:42 +0000

    Cloudflare用の設定？

--- nitro.config.ts
+++ nitro.config.ts
@@ -2,7 +2,12 @@ import { defineNitroConfig } from "nitropack/config"
 
 // https://nitro.build/config
 export default defineNitroConfig({
-  compatibilityDate: "latest",
+  compatibilityDate: "2026-03-07",
+  preset: "cloudflare_module",
+  cloudflare: {
+    deployConfig: true,
+    nodeCompat: true
+  },
   srcDir: "server",
   imports: false
 });
