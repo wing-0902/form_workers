ae6c1c5639b82e00ea950eb882eb06b64f753431
Wing <git@public.wing.osaka>
2026-03-07 07:44:30 +0000

    ex

--- server/routes/lifeis_money/form.post.ts
+++ server/routes/lifeis_money/form.post.ts
@@ -1,3 +1,7 @@
-import { eventHandler } from 'h3';
+import { eventHandler, readBody } from 'h3';
 
-export default eventHandler((event) => {});
+export default eventHandler( async (event) => {
+    const body = await readBody(event);
+
+    console.log(body);
+});
